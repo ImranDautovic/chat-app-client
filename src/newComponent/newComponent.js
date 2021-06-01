ee1a7056c15d492086e5476bee866415fdcd8699
ImranDautovic <48558211+ImranDautovic@users.noreply.github.com>
2021-06-01 22:58:36 +0200

    Initial Commit

---
+++ src/newComponent/newComponent.js
@@ -0,0 +1,14 @@
+import "./newComponent.css" 
+
+const NewComponent = (props) => {
+  
+    return (
+      <div>
+
+          IMRAN
+          
+      </div>
+    );
+  };
+  
+  export default NewComponent;
